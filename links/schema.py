453f7a7a3571dd95107b07bee828e6a16ee48df7
glyphack <sh.hooshyari@gmail.com>
2018-11-22 16:40:50 +0330

    [links]: added CreateLink mutation to schema.py

--- links/schema.py
+++ links/schema.py
@@ -9,8 +9,32 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
-class Query(graphene.ObjectType):
+class Query(graphene.AbstractType):
     links = graphene.List(LinkType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id = link.id,
+            url = link.url,
+            description=  link.description
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
