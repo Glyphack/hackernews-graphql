68982f80472644a672ec928e7a8dda3980dcf03c
glyphack <sh.hooshyari@gmail.com>
2018-11-22 16:41:30 +0330

    Added jwt to mutation

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,8 +1,22 @@
 import graphene
+import graphql_jwt
 
 import links.schema
+import users.schema
 
-class Query(links.schema.Query, graphene.ObjectType):
+
+class Query(links.schema.Query,
+            users.schema.Query,
+            graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query= Query)
+
+class Mutation(links.schema.Mutation,
+               users.schema.Mutation,
+               graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query= Query, mutation=Mutation)
