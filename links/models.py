bda4f98e1ce6997e8d1c5decb7984e28f033b5bf
glyphack <sh.hooshyari@gmail.com>
2018-11-26 15:04:21 +0330

    [link]: added vote model

--- links/models.py
+++ links/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth import get_user_model
+
 from django.db import models
 
 
 class Link(models.Model):
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
     url = models.URLField()
     description = models.TextField(blank=True)
+
+
+class Vote(models.Model):
+    url = models.ForeignKey(Link, related_name='vote', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
